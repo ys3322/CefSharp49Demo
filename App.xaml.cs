747122995865be5c7a8b8e2d93466804d8c7432a
Yangs <your.email@example.com>
2024-08-03 16:55:50 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace CefSharp49Demo
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
